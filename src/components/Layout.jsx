7ad2c4589d01807c8c33eb4cb21b4ce42b44dc07
Nehal <nehal@adcountymedia.com>
2026-08-19 02:37:42 +0530

    top on every route change

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,15 @@
 import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
 function Layout({ children }) {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
